c33f5d47dc709d8aa1c4af677ce206a2a582e155
Nuru Hussen <weddihaji@gmail.com>
2024-05-16 00:30:56 +0300

    console test

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -4,7 +4,7 @@ const Post = require("../models/Post");
 //CREATE POST
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
-
+  console.log(newPost);
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
