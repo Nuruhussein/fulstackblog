bcfadb5ee7f743f2059a4ed0278b667caa884eaf
Nuru Hussen <nuruhussen943@gmail.com>
2024-10-15 20:51:40 +0300

    changing to local

--- myblog/src/components/posts/Posts.jsx
+++ myblog/src/components/posts/Posts.jsx
@@ -1,13 +1,12 @@
 import Post from "../post/Post";
 import "./posts.css";
 
-export default function Posts({post}) {
+export default function Posts({ post }) {
   return (
     <div className="posts">
-         { post.map((p,index)=>{
-          return  <Post post={p} key={index}/>
-          })}
-    
+      {post.map((p, index) => {
+        return <Post post={p} key={index} />;
+      })}
     </div>
   );
 }
